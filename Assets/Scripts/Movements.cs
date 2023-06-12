d2eaf8fd9668d559ed4c87378f887a3733a490bb
Natasya Nadhira <natasyanadhira51@gmail.com>
2023-06-12 12:33:31 +0800

    add hologram, environment, movement and box collider

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movements : MonoBehaviour
 {
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class Movements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        _rb.velocity = dir * _speed;
         
     }
 }
